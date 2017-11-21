194936415da3c4fc119aef699e2b2851db55e169
Neils Nesse <nnesse@nthdimtech.com>
2017-11-20 23:15:07 -0800

    Move signetdev library interface over to firmware directory
    The firmware directory will soon be renamed to "core" or "base" so
    that this makes sense.

---
+++ signetdev/host/signetdev_unix.h
@@ -0,0 +1,14 @@
+#ifndef SIGNETDEV_UNIX_H
+#define SIGNETDEV_UNIX_H
+
+#include "common/signetdev_common.h"
+#include <pthread.h>
+
+//Platform specific
+void *transaction_thread(void *arg);
+
+extern pthread_t worker_thread;
+extern int g_command_pipe[];
+extern int g_command_resp_pipe[];
+
+#endif // SIGNETDEV_UNIX_H
