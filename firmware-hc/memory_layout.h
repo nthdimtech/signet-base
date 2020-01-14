55f721f6d5ea49b9b28f903576f490b07d6b8d4f
Neils Nesse <nnesse@nthdimtech.com>
2020-01-14 14:40:35 -0800

    Define structures for volume management and include profile definition info

--- firmware-hc/memory_layout.h
+++ firmware-hc/memory_layout.h
@@ -27,14 +27,23 @@
 #define EMMC_DB_KEYSTORE_BLOCK (EMMC_DB_FIRMWARE_UPDATE_BLOCK + EMMC_DB_FIRMWARE_UPDATE_BLOCKS)
 #define EMMC_DB_KEYSTORE_BLOCKS (4)
 #define EMMC_DB_FIRST_BLOCK (EMMC_DB_KEYSTORE_BLOCK + EMMC_DB_KEYSTORE_BLOCKS)
-#define EMMC_DB_NUM_BLOCK (1024)
+#define EMMC_DB_NUM_BLOCK (1024 + 4)
 #define EMMC_STORAGE_FIRST_BLOCK (EMMC_DB_NUM_BLOCK + EMMC_DB_FIRST_BLOCK)
 
+#define NUM_STORAGE_REGIONS (1024)
+#define MAX_TAGS (16)
+#define MAX_VOLUME_NAME_LEN (32)
+#define MAX_VOLUMES (10) //Includes "free space volume" and "primary volume" which can't be deleted
+
+enum hc_volumes {
+	VOL_FREE_SPACE,
+	VOL_PRIMARY_VIRTUAL,
+	VOL_CLIENT_STORAGE,
+};
+
 struct hcdb_tag {
-	u16 id;
+	u32 id;
 	u8 tag_name[MAX_TAG_NAME_LENGTH];
-	u16 user_data_size;
-	u8 user_data[];
 } __attribute__((packed));
 
 struct hcdb_profile {
@@ -43,22 +52,15 @@ struct hcdb_profile {
 	u16 id;
 	u16 parent_id;
 	u8 profile_name[MAX_PROFILE_NAME_LENGTH];
-	u16 num_tags;
+	u32 num_tags;
 	u16 tag_ids[MAX_PROFILE_TAGS];
-	u16 user_data_size;
-	u8 user_data[];
-} __attribute__((packed));
-
-struct hcdb_profile_definition_block_footer {
-	u16 num_profiles;
-	struct hcdb_profile profiles[];
 } __attribute__((packed));
 
 struct hcdb_profile_definition_block {
-	u16 num_tags;
-	struct hcdb_tag tags[];
-	// The structure hcdb_profile_definition_block_footer
-	// follows the tags. It stores the list of profiles
+	u32 num_tags;
+	struct hcdb_tag tags[MAX_TAGS];
+	u32 num_profiles;
+	struct hcdb_profile profiles[MAX_PROFILES];
 } __attribute__((packed));
 
 struct hcdb_profile_auth_data {
@@ -73,6 +75,23 @@ enum hc_firmware_upgrade_state {
 	HC_FILE_DOWNLOADED,
 	HC_BOOTLOADER_ONLY_UPGRADED,
 	HC_APPLICATION_ONLY_UPGRADED
+};
+
+#define HC_VOLUME_FLAG_READ_ONLY (1<<0)
+#define HC_VOLUME_FLAG_WRITABLE_ON_UNLOCK (1<<1)
+#define HC_VOLUME_FLAG_WRITABLE_ON_REQUEST (1<<2)
+#define HC_VOLUME_FLAG_HIDDEN (1<<3)
+#define HC_VOLUME_FLAG_VISIBLE_ON_UNLOCK (1<<4)
+#define HC_VOLUME_FLAG_VISIBLE_ON_REQUEST (1<<5)
+#define HC_VOLUME_FLAG_ONE_TIME_USE (1<<6)
+#define HC_VOLUME_FLAG_ENCRYPTED (1<<7)
+#define HC_VOLUME_FLAG_USE_KEYSTORE (1<<8)
+#define HC_VOLUME_FLAG_VIRTUAL (1<<9)
+
+struct hc_volume {
+	u32 flags;
+	u32 n_regions;
+	u8 volume_name[MAX_VOLUME_NAME_LEN];
 };
 
 struct hc_device_data {
@@ -93,7 +112,11 @@ struct hc_device_data {
 	u32 firmware_signature_pubkey[HC_FIRMWARE_SIGNATURE_PUBKEY_LEN];
 	u8 auth_random_cleartext[AUTH_RANDOM_DATA_LEN];
 	struct hcdb_profile_auth_data profile_auth_data[MAX_PROFILES];
-	u16 user_data_size;
+	struct hc_volume volumes[MAX_VOLUMES];
+	u16 storage_region_map[NUM_STORAGE_REGIONS];
+
+	struct hcdb_profile_definition_block profile_info; //Encrypted
+	u32 user_data_len;
 	u8 user_data[];
 } __attribute__((packed));
 
